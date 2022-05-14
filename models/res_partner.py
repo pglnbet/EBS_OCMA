eaa1ea510c706229e88c75fa80b11ffe732ceccd
dunmi9791 <dominic.anyanna@gmail.com>
2022-05-14 13:22:26 +0100

    views

--- models/res_partner.py
+++ models/res_partner.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api, _
-
-class Partner(models.Model):
-    _inherit = 'res.partner'
-
-    is_disco = fields.Boolean(string='Disco')
-    is_genco = fields.Boolean(string='Genco')
-    
-    vesting_contract = fields.Boolean(string='Vesting Contract')
-
-    contract_capacity_share = fields.Float('Contract Capacity Share')
-    
+# from odoo import models, fields, api, _
+#
+# class Partner(models.Model):
+#     _inherit = 'res.partner'
+#
+#     is_disco = fields.Boolean(string='Disco')
+#     # is_genco = fields.Boolean(string='Genco')
+#
+#     vesting_contract = fields.Boolean(string='Vesting Contract')
+#
+#     contract_capacity_share = fields.Float('Contract Capacity Share')
+#     
